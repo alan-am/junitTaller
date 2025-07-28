97cd1d3d92bbeed7c0989ef4d086b9cb105d3194
alan-am <alan.aguilardev@gmail.com>
2025-07-28 14:04:31 -0500

    test 1 constructor persona

---
+++ SistemaCitasMedicas/src/test/java/PacienteTest.java
@@ -0,0 +1,18 @@
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+import org.junit.jupiter.api.Test;
+
+public class PacienteTest {
+    @Test
+    void testCreacionPersona(){
+        //datos necesarios
+        String cedula = "0931978920";
+        String nombre = "Alan";
+        String correo = "alan@gmail.com";
+        //Creacion del objeto
+        Paciente paciente_resultado = new Paciente(cedula, nombre, correo);
+        //uso de assert
+        assertNotNull(paciente_resultado);
+    }
+}
