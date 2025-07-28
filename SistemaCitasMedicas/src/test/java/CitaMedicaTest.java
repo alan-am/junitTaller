63f35d548653130c9e1aacf789ed2ded2873c736
alan-am <alan.aguilardev@gmail.com>
2025-07-28 13:52:08 -0500

    junit

---
+++ SistemaCitasMedicas/src/test/java/CitaMedicaTest.java
@@ -0,0 +1,10 @@
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+public class CitaMedicaTest {
+    @Test
+    void test1(){
+        assertTrue(false);
+    }
+}
