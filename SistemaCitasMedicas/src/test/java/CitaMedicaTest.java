808ae79260790903cbd104e0150ffbd29e17dda6
Roberto Bravo <vicbravo@espol.edu.ec>
2025-07-28 14:09:20 -0500

    Implementando test en CitaMedicaTest

--- SistemaCitasMedicas/src/test/java/CitaMedicaTest.java
+++ SistemaCitasMedicas/src/test/java/CitaMedicaTest.java
@@ -1,10 +1,31 @@
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import org.junit.jupiter.api.Test;
+import java.time.LocalDateTime;
 
 public class CitaMedicaTest {
     @Test
-    void test1(){
-        assertTrue(false);
+    void testToString() {
+        Paciente paciente = new Paciente("09571834","Juan Perez","vitipro@gmail.com");
+        Medico medico = new Medico("Angela Lopez", "Cardiología");
+        LocalDateTime fechaHora = LocalDateTime.of(2024, 6, 10, 15, 30);
+        CitaMedica cita = new CitaMedica(paciente, medico, fechaHora);
+
+        String resultado = cita.toString();
+        String esperado = "Cita con Dra. Gomez para Juan Perez en 2024-06-10T15:30 [Agendada]";
+        assertEquals(esperado, resultado);
+    }
+
+    @Test
+    void testSetEstadoNull() {
+       Paciente paciente = new Paciente("09571834","Juan Perez","vitipro@gmail.com");
+        Medico medico = new Medico("Angela Lopez", "Cardiología");
+        LocalDateTime fechaHora = LocalDateTime.of(2024, 6, 10, 15, 30);
+        CitaMedica cita = new CitaMedica(paciente, medico, fechaHora);
+
+        assertThrows(IllegalArgumentException.class, () -> {
+            cita.setEstado(null);
+        });
     }
 }
