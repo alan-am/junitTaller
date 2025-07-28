55c59a0cd79a504c30e033a70c1dc5f116228413
Roberto Bravo <vicbravo@espol.edu.ec>
2025-07-28 14:20:47 -0500

    Update SistemaCitasTest

--- SistemaCitasMedicas/src/test/java/SistemasCitasTest.java
+++ SistemaCitasMedicas/src/test/java/SistemasCitasTest.java
@@ -1,3 +1,12 @@
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+import org.junit.jupiter.api.Test;
+
 public class SistemasCitasTest {
+    @Test
+    void testRegistrarPaciente() {
+        Paciente paciente = new Paciente("09334258172", "Juan Perez", "vitipro@gmail.com");
+        
+    }
 
 }
