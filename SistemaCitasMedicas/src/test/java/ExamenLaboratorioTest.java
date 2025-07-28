2b79a22d793512329a7fcb55b8aa178f99704a5c
MaycolMont <lv.geovanny@gmail.com>
2025-07-28 14:21:17 -0500

    Examen laboratorio tests

---
+++ SistemaCitasMedicas/src/test/java/ExamenLaboratorioTest.java
@@ -0,0 +1,29 @@
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+public class ExamenLaboratorioTest {
+    @Test
+    void testRegistrarResultado(){
+        Paciente paciente = new Paciente("0000000000", "Roberto Bravo", "robertobr@gmail.com");
+        ExamenLaboratorio examen = new ExamenLaboratorio(paciente, "Halter de corazon");
+
+        examen.registrarResultado("Positivo");
+
+        String esperado = "Examen: Halter de corazon - Paciente: Roberto Bravo ('00000') - Resultado: Positivo";
+        assertEquals(esperado, examen.toString());
+    }
+
+
+    @Test
+    void testToString(){
+
+        Paciente paciente = new Paciente("0000000001", "Alan Aguilar", "alan@gmail.com");
+        ExamenLaboratorio examen = new ExamenLaboratorio(paciente, "Sangre");
+        String salida = examen.toString();
+        assertTrue(salida.contains("Pendiente"));
+    }
+
+
+}
