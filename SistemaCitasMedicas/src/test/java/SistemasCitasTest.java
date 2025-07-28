24859277095dc5bd6b911b96f886095e7bc94476
Roberto Bravo <vicbravo@espol.edu.ec>
2025-07-28 14:28:08 -0500

    Hacer el testRegistrarPaciente

--- SistemaCitasMedicas/src/test/java/SistemasCitasTest.java
+++ SistemaCitasMedicas/src/test/java/SistemasCitasTest.java
@@ -1,11 +1,18 @@
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.jupiter.api.Test;
 
 public class SistemasCitasTest {
     @Test
     void testRegistrarPaciente() {
-        Paciente paciente = new Paciente("09334258172", "Juan Perez", "vitipro@gmail.com");
+        SistemaCitas sistema = new SistemaCitas();
+        sistema.registrarPaciente("09334258172", "Juan Perez", "vitipro@gmail.com");
+
+        Paciente paciente = sistema.buscarPaciente("09334258172");
+
+        assertEquals("Juan Perez", paciente.getNombre());
+        assertEquals("vitipro@gmail.com", paciente.getCorreo());
         
     }
 
